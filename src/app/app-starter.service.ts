5b6674b4876a2dbf91d20f2f0be19535a846c2b9
planctus <davidepuntoferrante@gmail.com>
2024-03-04 17:23:16 +0200

    chore(ci): Removing unused and breaking things

--- src/app/app-starter.service.ts
+++ src/app/app-starter.service.ts
@@ -18,51 +18,9 @@ export class AppStarterService {
     defaultUserPreferences: UserPreferences;
 
     constructor(
-        protected userService: UserService,
         protected i18nService: I18nService,
         @Inject(CONFIG_TOKEN) private config: EuiAppConfig,
         protected http: HttpClient,
     ) {
-    }
-
-    // eslint-disable-next-line
-    start(): Observable<any> {
-
-        return zip(
-            this.initUserService().pipe(
-                switchMap((userStatus) => {
-                    console.log(userStatus);
-                    return this.i18nService.init();
-                }),
-            ),
-        );
-    }
-
-    /**
-     * Fetches user details,
-     * create user: UserState object
-     * then initialise to the UserService on run time
-     */
-    // eslint-disable-next-line
-    initUserService(): Observable<any> {
-        return zip(
-            this.fetchUserDetails(),
-        ).pipe(
-            switchMap(([userDetails]) => this.userService.init(userDetails)));
-    }
-
-    /**
-     * Fetches user details
-     */
-    private fetchUserDetails(): Observable<UserDetails> {
-        // const url = this.config.modules.your_custom_module.your_custom_endpoint
-        const moduleCoreApi = this.config.modules.core;
-        const url = `${moduleCoreApi.base}${moduleCoreApi.userDetails}`;
-        const user = { userId: 'anonymous' };
-
-        if (!url) {
-            return of(user);
-        }
-        return this.http.get<UserDetails>(url);
     }
 }
