9e74a56993da71c43c3ed430c1eb85506937191f
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2024-03-06 21:24:52 +0200

    EWPP-3944: Lint.

--- src/app/features/module1/module1-routing.module.ts
+++ src/app/features/module1/module1-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Homepage } from './components/homepage/homepage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 const routes: Routes = [
-    { path: '', component: Homepage },
+    { path: '', component: HomepageComponent },
 ];
 
 @NgModule({
