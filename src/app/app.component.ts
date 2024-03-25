547ec74333e71c5dfe583a61d5e3e0843e4c584c
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2024-03-25 15:34:00 +0200

    EWPP-3990: Cleanup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,7 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-
-import {
-    EclMenuItemSelectEvent,
-} from '@eui/ecl';
-
+import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
-export class AppComponent implements OnDestroy {
-
-    // @ts-ignore
-    constructor(private store: Store<any>) {
-    }
-
-    ngOnDestroy() {
-        console.log('destroying');
-    }
-
-    onMenuItemSelected(evt: EclMenuItemSelectEvent) {
-        console.log('menu item selected', evt);
-    }
+export class AppComponent {
 }
